7fe4fa6a8a2bb2b6d2c57d21dc0d09f0fcc4ae1e
Echo2987 <levine.ethan1@gmail.com>
2023-03-27 13:27:22 -0400

    Initial Commit

---
+++ scripts/query.js
@@ -0,0 +1,23 @@
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database("./databases/trackrat.db", (err) => {
+	if (err) {
+		console.error(err.message);
+	}
+	console.log("Connected to the trackrat database.");
+});
+
+db.serialize(() => {
+	db.each(`SELECT * FROM tracking WHERE tracking_num = 1`, (err, row) => {
+		if (err) {
+			console.error(err.message);
+		}
+		console.log(row);
+	});
+});
+
+db.close((err) => {
+	if (err) {
+		console.error(err.message);
+	}
+	console.log("Close the database connection.");
+});
